d9190518cab870efd44a9cfceeb33f6c371d6ac1
emtiazzahid <emtiazzahid@gmail.com>
2016-10-06 21:05:52 +0600

    laravel installed

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('title')
+	Welcome
+@endsection
+
+@section('title')
+	<div class="row">
+		<div class="col-6">
+			<form action="#" method="post">
+				<div class="form-group">
+					<label for="email">Your email</label>
+					<input type="text" class="form-control" name="email" id="email">
+
+					
+				</div>
+			</form>
+		</div>
+	</div>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>@yield('title')</title>
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	@include('includes.header')
+	<div class="container">
+		@yield('content')
+	</div>
+	
+</body>
+</html>
